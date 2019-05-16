719081f9c408beacb2d5e8b1e8167bf8bf862131
maxblu <danydelaosa@gmail.com>
2019-05-16 02:02:57 -0400

    Tsukamoto ya pincha bien , infiere las variables de salidas.

--- start_system.py
+++ start_system.py
@@ -121,7 +121,142 @@ def start_washing_machine(pesoRopa,pH,tsukamoto):
     plot_membership.plot_funtions(valores_ropa,p1,p2,p3,valores_pH,ph1,ph2,ph3,valores_detergente,d1,d2,d3,valores_agua,a1,a2,a3)
     
 
+    #Inicializacion para variables a inferir
+    cantDetergPoco=-math.inf
+    cantDetergMedia=-math.inf
+    cantDetergMucha=-math.inf
+
+    cantDeAguaPoca=-math.inf
+    cantDeAguaMedia=-math.inf
+    cantDeAguaMucha=-math.inf
+
+    tsukamoto_pairs_deter=[]
+    tsukamoto_pairs_agua=[]
+    
+    #Reglas para la inferencia
+    #1
+    antecedente1=min(gradoPertencia_pH_acido, gradoPertenciaRopa_peso_ligera )
+    cantDetergMucha1=antecedente1
+    zi=inverse_membership(valores_detergente,d3,antecedente1)[0]
+    tsukamoto_pairs_deter.append((antecedente1,zi))
+
+    cantDeAguaMedia1=antecedente1
+    zi=inverse_membership(valores_agua,a2,antecedente1)[0]
+    tsukamoto_pairs_agua.append((antecedente1,zi))
+    
+
+    #2
+    antecedente2=min( gradoPertencia_pH_neutral,gradoPertenciaRopa_peso_ligera  )
+    cantDetergMedia2=cantDeAguaPoca2=antecedente2
+
+    zi =inverse_membership(valores_detergente,d2,antecedente2)[0]
+    tsukamoto_pairs_deter.append((antecedente2,zi))
+
+    zi=inverse_membership(valores_agua,a1,antecedente2)[0]
+    tsukamoto_pairs_agua.append((antecedente2,zi))
+
+
+    #3
+    antecedente3=min(gradoPertencia_pH_basico,gradoPertenciaRopa_peso_ligera )
+    cantDeAguaPoca3=cantDetergPoco3=antecedente3
+
+    zi=inverse_membership(valores_detergente,d1,antecedente3)[0]
+    tsukamoto_pairs_deter.append((antecedente3,zi))
+
+    zi=inverse_membership(valores_agua,a1,antecedente3)[0]
+    tsukamoto_pairs_agua.append((antecedente3,zi))
+
+
+    #4
+    antecedente4=min( gradoPertencia_pH_acido,gradoPertenciaRopa_peso_medio)
+    cantDeAguaMucha4=cantDetergMucha4=antecedente4
+
+    zi=inverse_membership(valores_detergente,d3,antecedente4)[0]
+    tsukamoto_pairs_deter.append((antecedente4,zi))
+
+    zi=inverse_membership(valores_agua,a3,antecedente4)[0]
+    tsukamoto_pairs_agua.append((antecedente4,zi))
+
+
+
+
+    #5
+    antecedente5=min( gradoPertencia_pH_neutral,gradoPertenciaRopa_peso_medio)
+    cantDetergMedia5=cantDeAguaMedia5=antecedente5
+
+    zi =inverse_membership(valores_detergente,d2,antecedente5)[0]
+    tsukamoto_pairs_deter.append((antecedente5,zi))
+
+    zi=inverse_membership(valores_agua,a2,antecedente5)[0]
+    tsukamoto_pairs_agua.append((antecedente5,zi))
+
+
+
+    #6
+    antecedente6=min(gradoPertencia_pH_basico,gradoPertenciaRopa_peso_medio)
+    cantDetergPoco6=cantDeAguaMedia6=antecedente6
+
+    zi=inverse_membership(valores_detergente,d1,antecedente6)[0]
+    tsukamoto_pairs_deter.append((antecedente6,zi))
+
+
+    zi=inverse_membership(valores_agua,a2,antecedente6)[0]
+    tsukamoto_pairs_agua.append((antecedente6,zi))
+
+    #7
+    antecedente7 =min (gradoPertencia_pH_acido,gradoPertenciaRopa_peso_pesado )
+    cantDetergMucha7=cantDeAguaMucha7=antecedente7
+
+    zi=inverse_membership(valores_detergente,d3,antecedente7)[0]
+    tsukamoto_pairs_deter.append((antecedente7,zi))
+
+    zi=inverse_membership(valores_agua,a3,antecedente7)[0]
+    tsukamoto_pairs_agua.append((antecedente7,zi))
+
+
+
+    #8
+    antecedente8= min (gradoPertencia_pH_neutral,gradoPertenciaRopa_peso_pesado)
+    cantDetergMedia8=cantDeAguaMucha8=antecedente8
+
+    zi =inverse_membership(valores_detergente,d2,antecedente8)[0]
+    tsukamoto_pairs_deter.append((antecedente8,zi))
+
+
+    zi=inverse_membership(valores_agua,a3,antecedente8)[0]
+    tsukamoto_pairs_agua.append((antecedente8,zi))
+
+
+    #9
+    antecedente9=min(gradoPertencia_pH_basico,gradoPertenciaRopa_peso_pesado) 
+    cantDetergPoco9=cantDeAguaMucha9=antecedente9
+
+    zi=inverse_membership(valores_detergente,d1,antecedente9)[0]
+    tsukamoto_pairs_deter.append((antecedente9,zi))
+
+    zi=inverse_membership(valores_agua,a3,antecedente9)[0]
+    tsukamoto_pairs_agua.append((antecedente9,zi))
+
+    #Aplicando el valor al consecuente(Revisar aqui si me quedo con el min o el max)
+    cantDeAguaPoca=max(cantDeAguaPoca2,cantDeAguaPoca3)
+    cantDeAguaMedia=max(cantDeAguaMedia1,cantDeAguaMedia5,cantDeAguaMedia6)
+    cantDeAguaMucha=max(cantDeAguaMucha4,cantDeAguaMucha7,cantDeAguaMucha8,cantDeAguaMucha9)
+
+    cantDetergPoco=max(cantDetergPoco3,cantDetergPoco6,cantDetergPoco9)
+    cantDetergMedia=max(cantDetergMedia2,cantDetergMedia5,cantDeAguaMedia6,cantDetergMedia8)
+    cantDetergMucha=max(cantDetergMucha1,cantDetergMucha4,cantDetergMucha7)
+
+    print(tsukamoto_pairs_deter)
+
+    print(tsukamoto_pairs_deter)
+    if tsukamoto:
+        print('Cantidad de detergente: ',tsukamoto_methot(tsukamoto_pairs_deter))
+        print('Cantidad de agua: ',tsukamoto_methot( tsukamoto_pairs_agua))
+
 
 
 if __name__ == "__main__":
-        start_washing_machine(5,7,True)
+        # start_washing_machine(5,7,True)
+
+        start_washing_machine(10,1,True)
+
